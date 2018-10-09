c5c0e61360337444b125d33f49ae368f4c9cabb8
kexincui <kexincuichina@gmail.com>
2018-10-09 05:31:16 +0000

    (HW3) Added tester comment so that user is seen on github

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rottenpotatoes::Application.routes.draw do
   resources :movies
   # map '/' to be a redirect to '/movies'
+  # Tester comment
   root :to => redirect('/movies')
   get 'movies/:id/same_director' => 'movies#same_director', :as => :same_director
 end
